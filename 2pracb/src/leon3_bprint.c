431cf34ee70ce0ad714aa4013028413c706ec2cd
opolo70 <opolo70@opolo70-PROX14-AMD>
2021-03-17 19:54:16 +0100

    Corregida. Sólo tienes comentarios de mejora.

--- 2pracb/src/leon3_bprint.c
+++ 2pracb/src/leon3_bprint.c
@@ -11,6 +11,11 @@
 int8_t leon3_print_string(char *c){
 	int pos = 0;
 	int a =1;
+	//OK, pero mejor si tratas errores 
+    //int8_t error=0;
+    //while(c[pos]!='\0'&&(!error)){
+      //error=leon3_putchar(c[pos]);
+	
 	while(a==1){
 		if(c[pos] != '\0'){
 		leon3_putchar(c[pos]);
@@ -21,6 +26,7 @@ int8_t leon3_print_string(char *c){
 		}
 
 	}
+	//return error;
 	return 0;
 }
 int8_t leon3_print_uint8(uint8_t c){
@@ -30,6 +36,22 @@ int8_t leon3_print_uint8(uint8_t c){
 	decenas = (c-centenas*100)/10;
     unidades = c - centenas*100 - decenas*10;
 
+//El código funciona bien, 
+	//pero se puede hacer de forma iterativa con un while 
+	//en vez de usar if else if... 
+	//Sería algo así
+	//int8_t error=0;
+	//int8_t first_digit=0;
+	
+	//if(i==0)
+	//	leon3_putchar('0');
+	//else{
+		//while(i&&(!error)){
+		// uint8_t aux=100; // aux para extraer dígitos
+		
+		// aux=aux/10; //centenas->decenas->unidades
+		//} 
+		
 	if(centenas>0) {
 		leon3_putchar('0' + centenas);
 		if(decenas == 0){
@@ -43,7 +65,7 @@ int8_t leon3_print_uint8(uint8_t c){
 
 	leon3_putchar('0' + unidades);
 
-
+	//return error; //trata el error
 	return 0;
 }
 
--- 2pracb/src/leon3_uart.c
+++ 2pracb/src/leon3_uart.c
@@ -9,6 +9,7 @@
 //! LEON3 UART A Transmit FIFO is FULL
 #define LEON3_UART_TFF (0x200)
 
+//OK pero acostumbrate a usar -> en vez de (*  ).
 #define leon3_UART_TF_IS_FULL() ((*pLEON3_UART_REGS).Status & LEON3_UART_TFF)
 
 //Estructura de datos que permite acceder a los registros de la
@@ -28,6 +29,7 @@ struct UART_regs
 
 };
 
+//El casting (struct   UART_regs *) te permite eliminar el warning
 
 struct UART_regs * pLEON3_UART_REGS = 0x80000100;
 
@@ -41,6 +43,7 @@ int8_t leon3_putchar(char c)
 	} //Espera mientras la cola de transmisión esté llena
 
 	if(write_timeout < 0xAAAAA){
+		//OK pero acostumbrate a usar -> en vez de (*  ).
 		(*pLEON3_UART_REGS).Data = c;
 	}
 	return (write_timeout == 0xAAAAA);
